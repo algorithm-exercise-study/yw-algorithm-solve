7f5adf9bd478633e79fb24d2bfa9e87c9d61461d
kywpcm <kywpcm@gmail.com>
2018-10-21 05:28:37 +0900

    - 재귀 문제 - dp 문제

---
+++ src/com/kywpcm/DP/P9095.java
@@ -0,0 +1,43 @@
+package com.kywpcm.DP;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P9095 {
+
+    private static int[] dp = new int[11];
+    private static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int line = s.nextInt();
+        while (line > 0) {
+            int n = s.nextInt();
+            list.add(sol(n));
+            line--;
+        }
+
+        for (Integer n : list)
+            System.out.println(n);
+
+        s.close();
+    }
+
+    private static int sol(int n) {
+        if (dp[n] != 0)
+            return dp[n];
+
+        if (n == 3)
+            return dp[n] = 4;
+        else if (n == 2)
+            return dp[n] = 2;
+        else if (n == 1)
+            return dp[n] = 1;
+
+        dp[n] = sol(n - 1) + sol(n - 2) + sol(n - 3);
+
+        return dp[n];
+    }
+
+}
---
+++ src/com/kywpcm/recursion/P10872.java
@@ -0,0 +1,41 @@
+package com.kywpcm.recursion;
+
+import org.junit.Test;
+
+/**
+ * <a href="https://www.acmicpc.net/problem/1914">https://www.acmicpc.net/problem/10872</a>
+ */
+public class P10872 {
+
+    private long factorial1(long n) {
+        if (n == 0)
+            return 1;
+
+        return n * factorial1(n - 1);
+    }
+
+    private long factorial2(long n) {
+        long res = 1;
+        for (; n >= 1; n--) {
+            res = res * n;
+        }
+        return res;
+    }
+
+    @Test
+    public void recursionTest() {
+//        long res = factorial1(9000);
+        long res = factorial1(100000);
+//        long res = factorial1(25);
+        System.out.println("result : " + res);
+    }
+
+    @Test
+    public void forStatementTest() {
+//        long res = factorial2(9000);
+        long res = factorial2(100000);
+//        long res = factorial2(25);
+        System.out.println("result : " + res);
+    }
+
+}
